64b4b6d68af25a59e5dfb9138ecb37b821db4a8c
Yalong <yalong@multicorewareinc.com>
2021-11-25 20:37:50 +0800

    This is Simulated Linux command on Window.

---
+++ Linux/usr/lib/perl5/core_perl/Storable/Limit.pm
@@ -0,0 +1,6 @@
+# bisected by stacksize
+$Storable::recursion_limit = 1090
+  unless defined $Storable::recursion_limit;
+$Storable::recursion_limit_hash = 631
+  unless defined $Storable::recursion_limit_hash;
+1;
